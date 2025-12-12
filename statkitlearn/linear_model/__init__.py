49e76ec2290e27262dfb36f46021a609a4ebb712
Krishna Arora <krishnaarora1029@gmail.com>
2025-12-12 11:28:46 +0530

    Remove LassoRegressor

--- statkitlearn/linear_model/__init__.py
+++ statkitlearn/linear_model/__init__.py
@@ -1,11 +1,9 @@
 from .sgd_regressor import SGDRegressor
 from .gd_regressor import GDRegressor
 from .ridge_regression import RidgeRegressor
-from .lasso_regressor import LassoRegressor
 
 __all__ = [
     "SGDRegressor",
     "GDRegressor",
-    "RidgeRegressor",
-    "LassoRegressor"
+    "RidgeRegressor"
 ]
